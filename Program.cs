7d175f03317cc18d606a82dc65d605166713a7d9
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2021-04-20 12:04:02 +0100

    my c# first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace commonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + , * , / , % , -
+            int numerator = 10;
+            int denomenator = 2;
+
+            int result = numerator / denomenator;
+            int result1 = numerator + denomenator;
+            int result2 = numerator - denomenator;
+            int result3 = numerator % denomenator;
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+
+            string myName = "Andrew";
+            string password = "1234";
+
+            string nameInput = Console.ReadLine();
+            string passwordInput = Console.ReadLine();
+
+            if(myName ==  nameInput && password == passwordInput )
+            {
+                Console.WriteLine("successful login .");
+            }
+            else{
+                Console.WriteLine("false");
+            }
+
+            //++ -- += (All increment operators)
+
+            int x = 20;
+            int y = 10;
+            y+=1;
+            //ternary operator
+            string results  = x > y ? $"{x} is greater than {y}": $"{x} is less than {y}";
+            string results1  = x > y ? $"{x} is greater than {y}": $"{x} is less than {y}";
+            string results2  = x > y ? $"{x} is greater than {y}": $"{x} is less than {y}";
+            
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
